779efb77c33d06ea710ffe552cdd6b2f2aae6d3b
NitishBaloria <76279474+NitishBaloria@users.noreply.github.com>
2022-10-06 13:18:57 +0530

    Add files via upload

---
+++ Activity-4/PythonBasics.py
@@ -0,0 +1,36 @@
+# Built-in functions
+
+# abs() function
+x=abs(-7.25)
+print(x)
+
+# len() function
+x=len("Hello")
+print(x)
+
+# min() function
+x=min(5,10)
+print(x)
+
+# max() function
+x=max(20,25)
+print(x)
+
+# round() function
+x=round(5.86)
+print(x)
+
+# isalnum() function
+txt="Company12"
+x=txt.isalnum()
+print(x)
+
+# type() function
+a=5
+print(a, "is of type", type(a))
+
+b=4.0
+print(b, "is of type", type(b))
+
+c=3+6j
+print(c, "is of type", type(c))
